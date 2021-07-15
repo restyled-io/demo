134f9fc97b823d0fcd8786a8c1c684d9c10344bb
Restyled.io <commits@restyled.io>
2021-07-15 07:07:11 +0000

    Restyled by fantomas

--- fantomas/test-file-1.fs
+++ fantomas/test-file-1.fs
@@ -1,9 +1,8 @@
-let Multiple9x9 () =
+let Multiple9x9() =
     for i in 1 .. 9 do
-        printf "\n";
+        printf "\n"
         for j in 1 .. 9 do
-            let k = i * j in
-            printf "%d x %d = %2d " i j k;
-        done;
-    done;;
-Multiple9x9 ();;
+            let k = i * j
+            printf "%d x %d = %2d " i j k
+
+Multiple9x9()
