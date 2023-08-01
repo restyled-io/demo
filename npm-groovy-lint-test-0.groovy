81a7601a46a209e03eef5beed3cf23c011352c1e
patrick brisbin <pbrisbin@gmail.com>
2023-08-01 10:04:32 -0400

    Update test case files

---
+++ npm-groovy-lint-test-0.groovy
@@ -0,0 +1,20 @@
+def variable = 1;
+
+pipeline {
+    agent any
+    options{ timestamps() }
+    stages{ stage('') {
+            steps {
+                sh """
+                """
+            }
+        }
+
+        stage('Deploy')
+        {
+            steps {
+                ansiColor('xterm') {
+                    sh """
+                    """
+                }
+            } } } }
---
+++ scalafmt-test-0.scala
@@ -0,0 +1,7 @@
+object a {
+  def c(b: List[Int]): List[Int] =
+    for {
+      a <- b
+      if ((a))
+    } yield a
+}
