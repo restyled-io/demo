f26ac2c5888fa4831dde3dc0de46367da128979b
Restyled.io <commits@restyled.io>
2024-07-24 14:51:08 +0000

    Restyled by verible

--- verible-test-0.sv
+++ verible-test-0.sv
@@ -1,13 +1,13 @@
-module debounce(
-  input wire logic clk, output logic debounced
-   );
-stateType ns;
+module debounce (
+    input wire logic clk,
+    output logic debounced
+);
+  stateType ns;
 
-  always_comb
-            begin
-             ns = ERR;
-       end
+  always_comb begin
+    ns = ERR;
+  end
 
   logic timerDone;
-     logic clrTimer;
-   endmodule
+  logic clrTimer;
+endmodule
